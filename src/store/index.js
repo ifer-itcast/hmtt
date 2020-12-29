0011af14318e24ee4c6a5a5bbebaae73dc359779
ifer-itcast <yangkangkang@itcast.cn>
2020-12-29 18:59:19 +0800

    设计分类和列表的模块

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import category from './modules/category'
+import list from './modules/list'
 
 Vue.use(Vuex)
 
@@ -11,5 +13,7 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
+    category,
+    list
   }
 })
