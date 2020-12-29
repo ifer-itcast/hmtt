c0fc830c15aac37a3d29abbe5e06600675be881f
ifer-itcast <yangkangkang@itcast.cn>
2020-12-29 21:39:38 +0800

    封装获取列表的action

--- src/store/modules/list.js
+++ src/store/modules/list.js
@@ -1,6 +1,19 @@
+import axios from 'axios'
+
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    allList: {} // { cateId1: [], cateId2: [] }
+  },
+  mutations: {
+    updateList(state, { currentCategoryId, list }) {
+      state.allList = { ...state.allList, [currentCategoryId]: list }
+    }
+  },
+  actions: {
+    async getList(context, cateId) {
+      const { data: { data: { results } } } = await axios.get(`http://ttapi.research.itcast.cn/app/v1_1/articles?channel_id=${cateId}&timestamp=${Date.now()}&with_top=1`)
+      context.commit('updateList', { currentCategoryId: cateId, list: results })
+    }
+  }
 }
